f7d9d407bead28fe7e96503d5ec0036aaf1337f1
Cristian Chuquiruna <cris.chuquiruna@gmail.com>
2023-08-01 19:52:43 -0300

    Base de Datos para el porfolio de Camila Caffieri

---
+++ index.mjs
@@ -0,0 +1,56 @@
+const PORT = 8000
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json('hi')
+})
+
+app.get('/acuarela', (req, res) => {
+  const API_KEY = process.env.API_KEY
+  const USER_ID = process.env.USER_ID
+  const photoset_id = '72177720309959399'
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${photoset_id}&user_id=${USER_ID}&privacy_filter=5&format=json&nojsoncallback=1`
+  
+  axios.request(url).then(response => {
+    res.json(response.data)
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
+app.get('/ilustracionBotanica', (req, res) => {
+  const API_KEY = process.env.API_KEY
+  const USER_ID = process.env.USER_ID
+  const photoset_id = '72177720309959379'
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${photoset_id}&user_id=${USER_ID}&privacy_filter=5&format=json&nojsoncallback=1`
+  
+  axios.request(url).then(response => {
+    res.json(response.data)
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
+app.get('/ceramica', (req, res) => {
+  const API_KEY = process.env.API_KEY
+  const USER_ID = process.env.USER_ID
+  const photoset_id = '72177720310186152'
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${photoset_id}&user_id=${USER_ID}&privacy_filter=5&format=json&nojsoncallback=1`
+  
+  axios.request(url).then(response => {
+    res.json(response.data)
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
