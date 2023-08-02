14c35825664a790021e17527a32be95e85bb6898
Cristian Chuquiruna <cris.chuquiruna@gmail.com>
2023-08-02 20:31:28 -0300

    Agregando Albun paraColorear

--- index.mjs
+++ index.mjs
@@ -53,4 +53,17 @@ app.get('/ceramica', (req, res) => {
   })
 })
 
+app.get('/paraColorear', (req, res) => {
+  const API_KEY = process.env.API_KEY
+  const USER_ID = process.env.USER_ID
+  const photoset_id = '72177720310227544'
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${photoset_id}&user_id=${USER_ID}&privacy_filter=5&format=json&nojsoncallback=1`
+  
+  axios.request(url).then(response => {
+    res.json(response.data)
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
