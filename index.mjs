93de8556916c7c39e72c62ebb8dc8ccb8ea187fb
Cristian <cris.chuquiruna@gmail.com>
2023-10-04 17:56:13 -0300

    add a new album called colaboracion

--- index.mjs
+++ index.mjs
@@ -27,6 +27,19 @@ app.get('/acuarela', (req, res) => {
   })
 })
 
+app.get('/colaboraciones', (req, res) => {
+  const API_KEY = process.env.API_KEY
+  const USER_ID = process.env.USER_ID
+  const photoset_id = '72177720311422420'
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${photoset_id}&user_id=${USER_ID}&privacy_filter=5&format=json&nojsoncallback=1`
+  
+  axios.request(url).then(response => {
+    res.json(response.data)
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
 app.get('/ilustracionBotanica', (req, res) => {
   const API_KEY = process.env.API_KEY
   const USER_ID = process.env.USER_ID
